24f4d19b014c7221802de6c97ac727d10dc1d6f7
ajbzuo <abalasubramaniam@zuora.com>
2025-02-11 18:07:57 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const app = express()
+const app = express();
 
 // Static Files
-app.use(express.static('public'))
-app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/img', express.static(__dirname + 'public/img'))
-app.use('/js', express.static(__dirname + 'public/js'))
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + '/public/css'));
+app.use('/img', express.static(__dirname + '/public/img'));
+app.use('/js', express.static(__dirname + '/public/js'));
 
 // Templating Engine
-app.set('views', './src/views')
-app.set('view engine', 'ejs')
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({ extended: true }))
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes
-const newsRouter = require('./src/routes/news')
+const newsRouter = require('./src/routes/news');
+app.use('/', newsRouter);
+app.use('/article', newsRouter);
 
-app.use('/', newsRouter)
-app.use('/article', newsRouter)
+// Error handling middleware (added to catch any issues)
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
 
 // Export the app for Vercel
-module.exports = app;  // Vercel uses this exported app to handle the routes
+module.exports = app;
